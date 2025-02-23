e5caa5d840267a7dcc3ddf06fda5f523cefe045c
4nd2ew <ondram20@gmail.com>
2025-02-23 15:26:45 +0100

    add easy_task_02

--- src/challenging_task_01/Main.java
+++ src/challenging_task_01/Main.java
@@ -1,4 +1,16 @@
 package challenging_task_01;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final double PI = 3.1415;
+        System.out.println("Zadej poloměr kruhu (cm):");
+        double polomer = Double.parseDouble(scanner.nextLine());
+        double obvod = 2 * PI * polomer;
+        double obsah = PI * polomer * polomer;
+        System.out.println("Obvod zadaného kruhu je: " + obvod + " cm");
+        System.out.println("Jeho obsah je: " + obsah + " cm^2");
+    }
 }
