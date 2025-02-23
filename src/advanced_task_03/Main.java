073f09ed345b910f0cda35f5bf399a7d17f7afec
4nd2ew <ondram20@gmail.com>
2025-02-23 16:04:37 +0100

    add advanced_task_03

---
+++ src/advanced_task_03/Main.java
@@ -0,0 +1,4 @@
+package advanced_task_03;
+
+public class Main {
+}
--- src/easy_task_03/Main.java
+++ src/easy_task_03/Main.java
@@ -1,4 +1,19 @@
 package easy_task_03;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Zadej své jméno:");
+        String name = scanner.nextLine();
+
+        if(name.length() < 3 || name.length() > 10){
+            System.out.println("Máš moc krátké nebo moc dlouhé jméno!");
+        }
+        else{
+            System.out.println("Normální jméno.");
+        }
+    }
 }
