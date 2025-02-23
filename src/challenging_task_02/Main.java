0554a640b7e21dc7c6ec7e2881f80daebcd4b80d
4nd2ew <ondram20@gmail.com>
2025-02-23 15:51:49 +0100

    add easy_task_03

--- src/challenging_task_02/Main.java
+++ src/challenging_task_02/Main.java
@@ -1,4 +1,13 @@
 package challenging_task_02;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadej řetězec:");
+        String text = scanner.nextLine();
+        text = text.toLowerCase();
+        System.out.println(text.contains("itnetwork"));
+    }
 }
---
+++ src/easy_task_03/Main.java
@@ -0,0 +1,4 @@
+package easy_task_03;
+
+public class Main {
+}
