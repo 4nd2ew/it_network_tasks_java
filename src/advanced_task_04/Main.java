f7b2d1c05134581cf42462b46233687dfeba4874
4nd2ew <ondram20@gmail.com>
2025-02-24 20:12:46 +0100

    add advanced_task_04

---
+++ src/advanced_task_04/Main.java
@@ -0,0 +1,4 @@
+package advanced_task_04;
+
+public class Main {
+}
--- src/easy_task_04/Main.java
+++ src/easy_task_04/Main.java
@@ -1,4 +1,14 @@
 package easy_task_04;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kolik ryb si dáš k večeři?");
+        int pocet = Integer.parseInt(scanner.nextLine());
+        for(int i = 0; i < pocet; i++){
+            System.out.println("<° )))-<");
+        }
+    }
 }
