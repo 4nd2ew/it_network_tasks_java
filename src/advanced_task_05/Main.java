4063c12ed99f3286e89966ecbbfd16a07bda1990
4nd2ew <ondram20@gmail.com>
2025-02-25 18:35:35 +0100

    add advanced_task_05

---
+++ src/advanced_task_05/Main.java
@@ -0,0 +1,4 @@
+package advanced_task_05;
+
+public class Main {
+}
--- src/easy_task_05/Main.java
+++ src/easy_task_05/Main.java
@@ -1,4 +1,12 @@
 package easy_task_05;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] pole = new int[10];
+
+        for(int i = 0; i < pole.length; i++)
+            pole[i] = pole.length - i;
+        for(int prvek: pole)
+            System.out.println(prvek);
+    }
 }
